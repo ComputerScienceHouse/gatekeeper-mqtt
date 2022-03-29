b8e40922e6ad42d5b668c42704b8e387d65f1f12
Galen Guyer <galen@galenguyer.com>
2022-03-29 17:54:44 -0400

    add route for impersonating users from association id

--- routes/memberProjects.js
+++ routes/memberProjects.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const ldap = require("../ldap");
+const impersonate = require("../impersonate");
 
 function findUser(id) {
   return new Promise((resolve, reject) => {
@@ -86,6 +87,56 @@ router.get("/by-key/:associationId", async (req, res) => {
   res.json({
     user: response,
   });
+});
+
+router.get("/impersonate/:associationId", async (req, res) => {
+  const key = await req.ctx.db.collection("keys").findOne({
+    [req.associationType]: {$eq: req.params.associationId},
+  });
+
+  if (!key) {
+    res.status(404).json({message: "Not found"});
+    return;
+  }
+
+  const userDocument = await req.ctx.db.collection("users").findOne({
+    id: {$eq: key.userId},
+    disabled: {$ne: true},
+  });
+  if (!userDocument) {
+    res.status(404).json({message: "User not found or disabled"});
+    return;
+  }
+
+
+  let user;
+  try {
+    user = await findUser(key.userId);
+  } catch (err) {
+    res.status(500).json({message: "Internal server error"});
+    return;
+  }
+
+  const response = {};
+  for (const attribute of user.attributes) {
+    if (attribute.type == "jpegPhoto") {
+      response[attribute.type] = attribute._vals[0].toString("base64");
+    } else {
+      const values = attribute._vals.map((value) => value.toString("utf8"));
+      if (ARRAYS.has(attribute.type)) {
+        response[attribute.type] = values;
+      } else {
+        if (values.length > 1) {
+          console.warn(`${attribute.type} has many values!!`);
+        }
+        response[attribute.type] = values.join(",");
+      }
+    }
+  }
+
+  const uid = response["uid"];
+
+  res.json(await impersonate.getImpersonationToken(uid));
 });
 
 module.exports = router;
